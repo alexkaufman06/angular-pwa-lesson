a4b44e8884e575c9280ee267b9ff4d3d7b07ef38
alexkaufman06 <alexkaufman06@gmail.com>
2020-04-15 19:13:52 -0700

    Tour of Heroes TDD

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,46 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let app: any;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
-      ],
+        AppComponent,
+        HeroesComponent
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular PWA'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular PWA');
+  it(`should have as title 'Tour of Heroes'`, () => {
+    expect(app.title).toEqual('Tour of Heroes');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+  it('should render the title in a h1 tag', () => {
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular PWA!!');
+    expect(compiled.querySelector('h1').textContent).toEqual('Tour of Heroes');
+  });
+
+  it('should render the the app-heroes tag', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('app-heroes').textContent).toEqual('Windstorm');
   });
 });
